e6ae4ad817c76540420d248c5a48cdeb50c3a0fe
Woochang Lee <changi112242@gmail.com>
2025-06-22 12:20:35 +0900

    style: 주문 상태 toggle 버튼 수정

--- src/layouts/PosLayout.jsx
+++ src/layouts/PosLayout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import styles from "./PosLayout.module.css";
 import PosHeader from "../components/common/PosHeader";
+import { useState } from "react";
 
 export default function PosLayout() {
+
+  const [isReceivingOrders, setIsReceivingOrders] = useState(false);
 
   return (
     <>
       <div className={styles.app}>
         <PosHeader
-          isReceivingOrders={true}
+          isReceivingOrders={isReceivingOrders}
           onToggle={() => setIsReceivingOrders((prev) => !prev)}
         />
         <main>
