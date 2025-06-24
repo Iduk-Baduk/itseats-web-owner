a070f978eb86e73ad52a1bc5b6849339114d924c
한상희 <tkdgml822@gmail.com>
2025-06-24 11:57:14 +0900

    feat: 주문별 필터링 구현

--- src/pages/pos/PosOrders.jsx
+++ src/pages/pos/PosOrders.jsx
@@ -1,16 +1,26 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import PosOrderItem from "../../components/pos/PosOrderCard";
 import PostSideBar from "../../components/pos/PosSideBar";
 import styles from "./PosOrders.module.css";
 
 export default function PosOrders() {
-  const order = useSelector((state) => state.order);
+  const orders = useSelector((state) => state.order);
+  const [currentTab, setCurrentTab] = useState("접수대기");
+
+  // 탭별로 주문 필터링
+  const filteredOrders = orders.filter((order) => {
+    if (currentTab === "접수대기") return order.deliveryStatus === "COKING";
+    if (currentTab === "진행중") return order.deliveryStatus === "DELIVERING";
+    if (currentTab === "주문내역") return order.deliveryStatus === "COMPLETED";
+    return true;
+  });
 
   return (
     <div className={styles.container}>
-      <PostSideBar />
+      <PostSideBar currentTab={currentTab} onSelect={setCurrentTab} />
       <div className={styles.content}>
-        {order.map((order) => (
+        {filteredOrders.map((order) => (
           <PosOrderItem key={order.orderNumber} order={order} />
         ))}
       </div>
