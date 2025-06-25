8a29a5a3b9e1bdaea53220ae1262f63968a2f89a
한상희 <tkdgml822@gmail.com>
2025-06-25 17:25:57 +0900

    feat: 로그인 UI 구현

--- src/pages/users/Login.jsx
+++ src/pages/users/Login.jsx
@@ -1,19 +1,49 @@
 import { Link } from "react-router-dom";
 
+import Button from "../../components/basic/Button";
+import TextInput from "../../components/basic/TextInput";
+
+import styles from "./Login.module.css";
+
 export default function Login() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <h1>로그인 페이지</h1>
-      <Link to="/sales">가맹점 페이지 이동 (임시)</Link>
-      <Link to="/register">회원가입 이동 (임시)</Link>
-    </div>
+    <>
+      <div className={styles.loginContainer}>
+        <h1 className={styles.title}>쿠팡이츠 Portal</h1>
+        <div className={styles.inputContainer}>
+          <div className={styles.input}>
+            <TextInput
+              name="login"
+              type="text"
+              onChange={() => {}}
+              placeholder="아이디"
+              maxLength={20}
+            />
+            <TextInput
+              name="password"
+              type="password"
+              onChange={() => {}}
+              placeholder="비밀번호"
+              maxLength={15}
+            />
+          </div>
+          <span style={{ opacity: 0.6 }}>ⓘ 영문, 숫자, 특수문자 포함 8~15자 </span>
+        </div>
+
+        <div className={styles.registerContainer}>
+          <div style={{ opacity: 0.6 }}>아직 계정이 없나요?</div>
+          <Link to="/register" className={styles.register}>
+            회원가입
+          </Link>
+        </div>
+
+        <Button
+          children="로그인"
+          onClick={() => {
+            alert("구현 필요");
+          }}
+        ></Button>
+      </div>
+    </>
   );
 }
