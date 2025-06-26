21aace46322432870fca398f5cc8bcf1dc0f3369
한상희 <tkdgml822@gmail.com>
2025-06-26 15:21:48 +0900

    feat: 가맹점 회원 회원가입 완료 UI 구현

---
+++ src/pages/users/register/RegisterCompleted.jsx
@@ -0,0 +1,27 @@
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../../components/basic/Button";
+import Stepper from "../../../components/register/Stepper";
+
+import styles from "./RegisterCompleted.module.css";
+
+export default function RegisterCompleted({ step, onNext }) {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.registerContainer}>
+      <Stepper currentStep={step} />
+
+      <h1>
+        <b>회원가입</b>이 완료되었습니다.
+      </h1>
+
+      <Button
+        children="로그인 하기"
+        onClick={() => {
+          if (onNext) onNext();
+          navigate("/login");
+        }}
+      />
+    </div>
+  );
+}
