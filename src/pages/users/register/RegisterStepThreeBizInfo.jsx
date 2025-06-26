9ffc23d10ed3e01429a9fc66d4d370ea43fa6c1b
한상희 <tkdgml822@gmail.com>
2025-06-26 15:20:33 +0900

    feat: 가맹점 회원 매장 등록, 정산 정보 입력 UI 구현

---
+++ src/pages/users/register/RegisterStepThreeBizInfo.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Button from "../../../components/basic/Button";
+import TextInput from "../../../components/basic/TextInput";
+import Stepper from "../../../components/register/Stepper";
+
+import styles from "./RegisterStepThreeBizInfo.module.css";
+
+export default function RegisterStepThreeBizInfo({ step, data, onSubmit }) {
+  const [values, setValues] = useState(Array(data.length).fill(""));
+
+  const handleChange = (index, value) => {
+    const newValues = [...values];
+    newValues[index] = value;
+    setValues(newValues);
+  };
+
+  const allFilled = values.every((v) => v && v.trim() !== "");
+
+  return (
+    <div className={styles.registerContainer}>
+      <Stepper currentStep={step} />
+
+      <h1>
+        <b>매장 등록, 정산에</b>필요한 정보를 등록해주세요.
+      </h1>
+
+      {data?.map((field, idx) =>
+        field.type === "textarea" ? (
+          <textarea
+            key={idx}
+            value={values[idx]}
+            placeholder={field.placeholder}
+            onChange={(e) => handleChange(idx, e.target.value)}
+            className={styles.textarea}
+          />
+        ) : (
+          <TextInput
+            key={idx}
+            value={values[idx]}
+            placeholder={field.placeholder}
+            onChange={(e) => handleChange(idx, e.target.value)}
+          />
+        )
+      )}
+      <Button children="다음" onClick={() => onSubmit(values)} disabled={!allFilled} />
+    </div>
+  );
+}
